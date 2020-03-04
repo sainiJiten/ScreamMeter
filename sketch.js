ce5f47c598323ed5d3752666ee20f0f22a86f749
sainiJiten <saini.jiten10@gmail.com>
2020-03-04 16:59:02 -0500

    second commit

--- sketch.js
+++ sketch.js
@@ -1,11 +1,29 @@
-var mic;
-function setup(){
-mic = new p5.AudioIn();
-mic.start();
-}
-function draw(){
-	var vol = mic.getLevel();
-	console.log(vol);
+var mic
+var vol = 0
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // Create an Audio input
+  mic = new p5.AudioIn();
+     // start the Audio Input.
+  // By default, it does not .connect() (to the computer speakers)
+  mic.start();
 }
 
+function draw() {
+  background(200);
 
+  // Get the overall volume (between 0 and 1.0)
+   var vol = mic.getLevel();
+  
+  fill(127);
+  stroke(0);
+  
+  // Draw an ellipse with height based on volume
+  var h = map(vol, 0, 0.5, height, 0);
+  ellipse(width/2, h - 25, 50, 50);
+}
+
+function touchStarted() {
+  getAudioContext().resume()
+}
