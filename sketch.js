7901cdc442177a419afcc74666b3460f8a55aeae
sainiJiten <saini.jiten10@gmail.com>
2020-03-05 09:02:59 -0500

    Changed definition of sine

--- sketch.js
+++ sketch.js
@@ -13,9 +13,11 @@ function draw() {
 	  i = 0;
   }
   background(000);
-  factor = Math.sin(2*Math.PI*5000*i);
+  factor = Math.sin(i);
+  console.log(factor);
   var vol = mic.getLevel();
   var h = height/2 - vol*factor*height/2;
+//  var h = height/2 - vol*height/2;
   fill(0,255,0);
   stroke(0,255,0);
   ellipse(width/2, h , 50, 50);
